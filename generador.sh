2943df4fc275c9cabd3d4baf05459803073220eb
Sofia Grando <sofi.grando@gmail.com>
2023-12-19 23:41:37 -0300

    funciona pero no pone hash a los audios

--- generador.sh
+++ generador.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
+
+generar_archivo() {
+    VAR=$((RANDOM % 3))
+
+    if [ "$VAR" -eq 0 ]; then
+         ffmpeg -f lavfi -i "anoisesrc=a=0.1:c=white:duration=5" "$output_directory/audio_$i.wav"
+        contenido="$output_directory/audio_$i.wav"
+    elif [ "$VAR" -eq 1 ]; then
+        convert -size 100x100 xc: +noise Random noise.png
+        contenido=$(cat noise.png)
+        rm noise.png
+    elif [ "$VAR" -eq 2 ]; then
+        base64 /dev/urandom | head -c 500 > file.txt
+        contenido=$(cat file.txt)
+        rm file.txt
+    fi
+    
+    hash=$(echo "$contenido" | md5sum | awk '{print $1}')  # Calcula el hash MD5 del contenido
+    echo "$hash"
+    nombre_archivo="$output_directory/$hash"  # Construye el nombre del archivo con el hash
+    
+    echo "$contenido" > "$nombre_archivo"  # Guarda el contenido en el archivo
+    
+    echo "Archivo generado: $nombre_archivo"
+}
+
 output_directory="outputs"
-mkdir -p "$output_directory"
+mkdir -p "$output_directory"  # Crea la carpeta si no existe
 
-VAR=$((RANDOM % 3))
-
-if [ "$VAR" -eq 1 ]; then
-    archivo="ffmpeg -f lavfi -i 'anoisesrc=a=0.1:c=white:duration=5'"
-elif [ "$VAR" -eq 2 ]; then
-    archivo="convert -size 100x100 xc: +noise Random"
-elif [ "$VAR" -eq 3 ]; then
-    archivo="base64 /dev/urandom | head -c 500 >"
-else
-    echo "Valor no esperado en VAR: $VAR"
-fi
-
-nombre=md5sum "$archivo"
-
-"$archivo""output_directory/$nombre"
-
-echo "Archivos generados exitosamente en el directorio $output_directory."
+# Genera cinco archivos llamando a la función cinco veces
+for i in {1..10}; do
+    generar_archivo
+done
