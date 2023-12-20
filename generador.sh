765a546d70f01d880b4c31d5053fddd8c81c669a
Sofia Grando <sofi.grando@gmail.com>
2023-12-19 22:39:41 -0300

    algo que no funciona

--- generador.sh
+++ generador.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+output_directory="outputs"
+mkdir -p "$output_directory"
 
-$VAR = $echo (($RANDOM%3))
-if $VAR = 1
-ffmpeg -f lavfi -i "anoisesrc=a=0.1:c=white:duration=5" out.wav
-if $VAR = 2
-convert -size 100x100 xc: +noise Random noise.png
-if VAR = 3
-base64 /dev/urandom | head -c 500 > file.txt 
+VAR=$((RANDOM % 3))
+
+if [ "$VAR" -eq 1 ]; then
+    archivo="ffmpeg -f lavfi -i 'anoisesrc=a=0.1:c=white:duration=5'"
+elif [ "$VAR" -eq 2 ]; then
+    archivo="convert -size 100x100 xc: +noise Random"
+elif [ "$VAR" -eq 3 ]; then
+    archivo="base64 /dev/urandom | head -c 500 >"
+else
+    echo "Valor no esperado en VAR: $VAR"
+fi
+
+nombre=md5sum "$archivo"
+
+"$archivo""output_directory/$nombre"
+
+echo "Archivos generados exitosamente en el directorio $output_directory."
